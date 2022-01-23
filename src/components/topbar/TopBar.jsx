77dcf455a1f66d18e13b21590e89bb2dac253b34
Sushmitha-94 <st.sushmi@gmail.com>
2022-01-23 14:31:36 -0600

    Mail id TopBar

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,13 +1,22 @@
 import "./topbar.scss";
+import {Mail} from '@material-ui/icons';
 
 export default function TopBar() {
   return <div className="topbar">
       <div className="wrapper">
           <div className="left">
             <a href="#intro" className="logo">s</a>
+            <div className="itemContainer">
+              <Mail className="icon"/>
+              <span>st.sushmi@gmail.com</span>
+            </div>
           </div>
           <div className="right">
-            right
+            <div className="hamburger">
+              <span className="line1"></span>
+              <span className="line2"></span>
+              <span className="line3"></span>
+            </div>
           </div>
       </div>
   </div>;
